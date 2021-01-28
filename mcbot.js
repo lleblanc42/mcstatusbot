45a7eefec67a2df2f3664b3b153e6105cd98180b
iDoesNotCompute <luke@badfngamers.com>
2021-01-28 00:47:10 -0700

    Bug fixes/restructuring

--- mcbot.js
+++ mcbot.js
@@ -7,13 +7,11 @@ const fs = require('fs');
 const client = new Discord.Client();
 const settings = require('./config.json');
 
-var hasIcon = 'n/a';
-pingFrequency = (settings.pingInterval * 1000);
-embedColor = ("0x" + settings.embedColor);
+var pingFrequency = (settings.pingInterval * 1000);
 
 function getDate() {
-    date = new Date();
-    cleanDate = date.toLocaleTimeString();
+    var date = new Date();
+    var cleanDate = date.toLocaleTimeString();
 }
 
 function getServerStatus() {
@@ -21,33 +19,25 @@ function getServerStatus() {
         getDate();
 
         if (!(typeof err === 'undefined' || err === null)) {
-            msg = settings.serverStatus.dnd.msg;
-            type = settings.serverStatus.dnd.type;
+            var msg = settings.serverStatus.dnd.msg;
+            var type = settings.serverStatus.dnd.type;
 
             client.user.setStatus('dnd');
             client.user.setActivity(msg, { type: type });
 
             console.log((chalk.yellow('\[' + cleanDate + '\]:') + chalk.white(' Ping: ' + 'Error getting server status')));
             console.error(err);
-        } else if (typeof res.players.sample === 'undefined') {
-            msgPrefix = settings.serverStatus.idle.msg.preffix;
-            msgSuffix = settings.serverStatus.idle.msg.suffix;
-            type = settings.serverStatus.idle.type;
-            enabled = settings.serverStatus.idle.enabled;
-            playerCount = res.players.online + ' / ' + res.players.max;
-
-            if (enabled === 1) client.user.setStatus('idle');
-
-            client.user.setActivity(msgPrefix + playerCount + msgSuffix, { type: type }).then(presence => console.log(
-                chalk.cyan('\[' + cleanDate + '\]:') + chalk.white(' Ping: ' + playerCount)
-            )).catch(console.error);
         } else {
-            msgPrefix = settings.serverStatus.online.msg.preffix;
-            msgSuffix = settings.serverStatus.online.msg.suffix;
-            type = settings.serverStatus.online.type;
-            playerCount = res.players.online + ' / ' + res.players.max;
+            var msgPrefix = (typeof res.players.sample === 'undefined' ? settings.serverStatus.idle.msg.preffix : settings.serverStatus.online.msg.preffix);
+            var msgSuffix = (typeof res.players.sample === 'undefined' ? settings.serverStatus.idle.msg.suffix : settings.serverStatus.online.msg.suffix);
+            var type = (typeof res.players.sample === 'undefined' ? settings.serverStatus.idle.type : settings.serverStatus.online.type);
+            var playerCount = res.players.online + ' / ' + res.players.max;
 
-            client.user.setStatus('online');
+            if (typeof res.players.sample === 'undefined') {
+                var enabled = settings.serverStatus.idle.enabled;
+
+                if (enabled === 1) client.user.setStatus('idle');
+            } else client.user.setStatus('online');
 
             client.user.setActivity(msgPrefix + playerCount + msgSuffix, { type: type }).then(presence => console.log(
                 chalk.cyan('\[' + cleanDate + '\]:') + chalk.white(' Ping: ' + playerCount)
