ca2bdf0cd7352f2dc95d8e3e7eb7cb5f33460e20
iDoesNotCompute <luke@badfngamers.com>
2021-01-28 15:50:36 -0700

    Updated start.sh for use in Pterodactyl egg

--- start.sh
+++ start.sh
@@ -1,4 +1,14 @@
 # /bin/sh
+
+if [ $# -eq 2 ] && [ $1 -eq "-autoupdate" ]; then
+    shift
+
+    if [ $1 -eq 1 ]; then
+        git pull
+        /usr/local/bin/npm update
+    fi
+fi
+
 until node mcbot.js; do
     echo "Bot stopped with exit code $?. Restarting..." >&2
     sleep 1
