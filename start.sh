1bfd3810891919972f0069419f787b9d9bb82aed
iDoesNotCompute <luke@badfngamers.com>
2021-05-12 20:43:38 -0600

    Temporarily removed auto update features

--- start.sh
+++ start.sh
@@ -13,14 +13,14 @@ fi
 
 echo $$ > ${LOCKFILE}
 
-if [ $1 -eq 1 ]; then
-    echo "Auto updating..."
-
-    sudo apk add git
-
-    git pull
-    /usr/local/bin/npm update
-fi
+#if [ $1 -eq 1 ]; then
+#    echo "Auto updating..."
+#
+#    sudo apk add git
+#
+#    git pull
+#    /usr/local/bin/npm update
+#fi
 
 until node mcbot.js; do
     echo "Bot stopped with exit code $?. Restarting..." >&2
